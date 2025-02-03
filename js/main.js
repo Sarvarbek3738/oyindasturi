fe81317fea0a829b27f9a1bee6616ddec626ac3b
Sarvarbek <sarvarbekodilxojayev898@gmail.com>
2025-02-03 11:09:35 +0500

    update

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-let randomText = "none"
 const word = document.querySelector("#word")
 const timer = document.querySelector("#timer")
 const inputmode = document.querySelector(".inputmode")
@@ -7,16 +6,26 @@ const record = document.querySelector(".record")
 const select = document.querySelector("#select")
 let score = 0
 let ball = 0
-let HightScore = localStorage.getItem("HightScore") ? JSON.parse(localStorage.getItem("HightScore")) : 0;
-let internal
 let time = 0
-const words = [
-    "abreact", "abreacted", "abreacting", "abreaction", "abreactions", "paltrier", "paltriest", "paltrily", "paltriness", "paltrinesses", "paltry", "paludal", "paludism", "shiv", "shiva", "shivah", "shivahs", "shivaree", "shivareed", "shivareeing",
-]
+let internal
+let HightScore = localStorage.getItem("HightScore") ? JSON.parse(localStorage.getItem("HightScore")) : 0;
+// const words = [
+//     "abreact", "abreacted", "abreacting", "abreaction", "abreactions", "paltrier", "paltriest", "paltrily", "paltriness", "paltrinesses", "paltry", "paludal", "paludism", "shiv", "shiva", "shivah", "shivahs", "shivaree", "shivareed", "shivareeing",
+// ]
+
+const apiLink = `https://random-word-api.herokuapp.com/word`
+let randomWord;
+const getData = async (link) => {
+    const req = await fetch(link);
+    const data = await req.json();
+    randomWord = data[0]
+    console.log(randomWord);
+    textWord()
+};
+getData(apiLink)
+
 const textWord = () => {
-    let random = Math.floor(Math.random() * words.length)
-    randomText = words[random]
-    word.textContent = randomText
+    word.textContent = randomWord;
 }
 const timerWord = () => {
     internal = setInterval(() => {
@@ -24,14 +33,14 @@ const timerWord = () => {
         if (time == 0) {
             word.textContent = "Yutqizdingiz !!!"
             clearInterval(internal)
-            // textWord()
         }
         time--
     }, 1000)
 }
 record.textContent = HightScore
 inputmode.addEventListener("input", () => {
-    if (inputmode.value == randomText) {
+    if (inputmode.value ==  word.textContent) {
+        getData(apiLink)
         time += ball
         score += 1
         yutuq.textContent = score
@@ -65,7 +74,6 @@ select.addEventListener("click", () => {
         textWord()
         timerWord()
     }
-
 })
 
 
